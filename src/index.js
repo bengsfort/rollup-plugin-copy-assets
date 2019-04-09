62bb958404848445c276269ea14ca6e37c59c842
Matt Bengston <matt.bengston@epicgames.com>
2019-04-09 06:11:45 -0400

    Upgrade all dependencies, add prettier and pre-commit formatting hooks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-import fs from 'fs-extra';
-import path from 'path';
+import fs from "fs-extra";
+import path from "path";
 
 /**
  * Takes an array of files/directories to copy to the final build directory.
@@ -10,22 +10,26 @@ import path from 'path';
  */
 export default function copy(options = {}) {
   const { assets } = options;
-  let basedir = '';
+  let basedir = "";
   return {
-    name: 'copy-assets',
+    name: "copy-assets",
     options: function options(options) {
       // Cache the base directory so we can figure out where to put assets.
       basedir = path.dirname(options.input);
     },
     generateBundle: ({ file, dir }) => {
       const outputDirectory = dir || path.dirname(file);
-      return Promise.all(assets.map((asset) => fs.copy(
-        asset,
-        path.join(outputDirectory, path.relative(basedir, asset)),
-        {
-          overwrite: true,
-        }
-      )));
+      return Promise.all(
+        assets.map(asset =>
+          fs.copy(
+            asset,
+            path.join(outputDirectory, path.relative(basedir, asset)),
+            {
+              overwrite: true,
+            }
+          )
+        )
+      );
     },
   };
 }
--- test/tests.js
+++ test/tests.js
@@ -1,70 +1,74 @@
-import assert from 'assert';
-import { rollup } from 'rollup';
-import fs from 'fs-extra';
-import copy from '../dist/rollup-plugin-copy-assets.module';
+import assert from "assert";
+import { rollup } from "rollup";
+import fs from "fs-extra";
+import copy from "../dist/rollup-plugin-copy-assets.module";
 
 // Change working directory to current
 process.chdir(__dirname);
 
-describe('rollup-plugin-copy-assets', function() {
-  afterEach(() => fs.remove('output'));
+describe("rollup-plugin-copy-assets", function() {
+  afterEach(() => fs.remove("output"));
 
-  it('should copy the assets to output dir', function(done) {
-    build({ assets: [ 'fixtures/top-level-item.txt' ] })
-      .then(() => Promise.all([
-        assertExists('output/top-level-item.txt'),
-      ]))
+  it("should copy the assets to output dir", function(done) {
+    build({ assets: ["fixtures/top-level-item.txt"] })
+      .then(() => Promise.all([assertExists("output/top-level-item.txt")]))
       .then(() => done());
   });
 
-  it('should copy folders to output dir with same hierarchy', function(done) {
+  it("should copy folders to output dir with same hierarchy", function(done) {
     build({
-      assets: [ 'fixtures/assets' ],
-    }).then(() => Promise.all([
-      assertExists('output/assets'),
-      assertExists('output/assets/foo.txt'),
-      assertExists('output/assets/bar.csv'),
-    ]))
-    .then(() => done());
+      assets: ["fixtures/assets"],
+    })
+      .then(() =>
+        Promise.all([
+          assertExists("output/assets"),
+          assertExists("output/assets/foo.txt"),
+          assertExists("output/assets/bar.csv"),
+        ])
+      )
+      .then(() => done());
   });
 
-  it('should copy all paths given to it', function(done) {
+  it("should copy all paths given to it", function(done) {
     build({
-      assets: [ 'fixtures/assets', 'fixtures/top-level-item.txt' ],
-    }).then(() => Promise.all([
-      assertExists('output/assets'),
-      assertExists('output/assets/foo.txt'),
-      assertExists('output/assets/bar.csv'),
-    ]))
-    .then(() => done());
+      assets: ["fixtures/assets", "fixtures/top-level-item.txt"],
+    })
+      .then(() =>
+        Promise.all([
+          assertExists("output/assets"),
+          assertExists("output/assets/foo.txt"),
+          assertExists("output/assets/bar.csv"),
+        ])
+      )
+      .then(() => done());
   });
 
-  it('should not fail when the directory exists', function(done) {
-    const doBuild = () => build({
-      assets: ['fixtures/assets/foo.txt'],
-    }).then(() => Promise.all([
-      assertExists('output/assets/foo.txt'),
-    ]));
-    doBuild().then(() => doBuild()).then(() => done());
+  it("should not fail when the directory exists", function(done) {
+    const doBuild = () =>
+      build({
+        assets: ["fixtures/assets/foo.txt"],
+      }).then(() => Promise.all([assertExists("output/assets/foo.txt")]));
+    doBuild()
+      .then(() => doBuild())
+      .then(() => done());
   });
 });
 
 // Run the rollup build with an plugin configuration.
 function build(config) {
   return rollup({
-    input: './fixtures/index.js',
-    plugins: [
-      copy(config),
-    ],
-  }).then(bundle => bundle.write({
-    file: 'output/bundle.js',
-    format: 'iife',
-    name: 'test',
-  }));
+    input: "./fixtures/index.js",
+    plugins: [copy(config)],
+  }).then(bundle =>
+    bundle.write({
+      file: "output/bundle.js",
+      format: "iife",
+      name: "test",
+    })
+  );
 }
 
 // Asserts that a file does or does not exist.
 function assertExists(file, shouldExist = true) {
-  return fs.pathExists(file)
-    .then((exists) => assert.ok(exists === shouldExist));
+  return fs.pathExists(file).then(exists => assert.ok(exists === shouldExist));
 }
